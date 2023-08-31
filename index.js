79276cd2fff78e1f5f3978072cbc2f9269705d66
0laolu <abdullateefolaolu@gmail.com>
2023-08-31 16:13:56 +0100

    style: finish layout for form-section

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 
+// function doo(event) {
+//     if(isNaN(event.key) && event.key !== "Backspace") {
+//         event.preventDefault()
+//         return false
+//     }
+// }
 
-function foo(event, previousInput, currentInput, nextInput) {
-    const length = document.getElementById(currentInput).value.length
-    const maxLength = document.getElementById(currentInput).getAttribute("maxlength")
+// function moveInputFocus(event, previousInput, currentInput, nextInput) {
+//     const length = document.getElementById(currentInput).value.length
+//     const maxLength = document.getElementById(currentInput).getAttribute("maxlength")
 
-    if(length == maxLength) {
-        if(nextInput !== '') {
-            document.getElementById(nextInput).focus()
-        }
-    }
+//     console.log(event.key)
+//     console.log(event)
+//     if(length == maxLength) {
+//         if(nextInput !== '') {
+//             document.getElementById(nextInput).focus()
+//         }
+//     }
 
-    if(event.key === "Backspace" && length == 0) {
-        if(previousInput !== '') {
-            document.getElementById(previousInput).focus()
-        }
-    }
-}
+//     if(event.key === "Backspace" && length == 0) {
+//         if(previousInput !== '') {
+//             document.getElementById(previousInput).focus()
+//         }
+//     }
+
+    
+// }
